37d43755f67d5dc919b13bc108a5b9450cef7da6
alexgeretz <ageretz@icloud.com>
2026-06-01 02:41:33 -0500

    Update card.h

--- card.h
+++ card.h
@@ -6,12 +6,6 @@
   and the data structure for a playing card.
 
   Each card has a suit and a rank.
-
-  Deck is a linked list with a dummy head; any function parameters calling
-  the Deck take this into account.
-  Each Deck contains a pointer to a Card head, a pointer to a
-  Card tail, and a pointer to the next Inoq_list.
-  Pointers are used to bound the memory allocation required for an Inoq_list.
 */
 
 typedef enum
